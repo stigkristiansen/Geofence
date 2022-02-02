03dc17ac37830ba9ba9e41fea7faff12b4050277
stigkristiansen <stigkristiansen@outlook.com>
2022-02-02 22:16:50 +0100

    Update module.php

--- Controller/module.php
+++ Controller/module.php
@@ -7,7 +7,7 @@ class GeofenceController extends IPSModule {
     public function Create(){
 		$instances = IPS_GetInstanceListByModuleID('{C5271BF2-DDC9-4EA7-8467-A8C645500263}');
 		$count = count($instances);
-		if($count>0 && $instances[0]!=$this->InstanceID) {
+		if($count==2) {
 			throw new Exception('The Geofence Controller already exists!');
 		}
 
